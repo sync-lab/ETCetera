9a727eaec30edb134ec5399d71b3e84fde90e367
Ivo <I.vanStraalen@student.tudelft.nl>
2021-05-06 15:33:34 +0200

    Add global save path

--- config.py
+++ config.py
@@ -1,6 +1,12 @@
-path = None
+import os.path
+
+path = '/home/ivo/Master Thesis Libs/dreal4/dreal'
 #dreal_path = '/Users/gdelimpaltadak/dReal-3.16.06.02-darwin/bin/dReal'
-dreal_path = None
+dreal_path = '/home/ivo/Master Thesis Libs/dreal4/dreal'
 #dreach_path = '/Users/gdelimpaltadak/dReal-3.16.06.02-darwin/bin/dReach'
-dreach_path = None
-flowstar_path = None
+dreach_path = '/home/ivo/Master Thesis Libs/dReal-3.16.06.02-linux/bin/dReach'
+flowstar_path = '/home/ivo/Master Thesis Libs/flowstar-2.1.0/flowstar-2.1.0'
+
+
+root_path = os.path.dirname(os.path.abspath(__file__))
+save_path = os.path.join(root_path, './saves')
