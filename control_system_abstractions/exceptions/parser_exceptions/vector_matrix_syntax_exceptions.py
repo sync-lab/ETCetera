82569c99f72836b2a6232ecac0217d5aebd6d095
Ivo <I.vanStraalen@student.tudelft.nl>
2021-05-20 13:44:41 +0200

    More detailed Exceptions

--- control_system_abstractions/exceptions/parser_exceptions/vector_matrix_syntax_exceptions.py
+++ control_system_abstractions/exceptions/parser_exceptions/vector_matrix_syntax_exceptions.py
@@ -1,36 +1,47 @@
-class IncorrectMatrixBoundaryException(Exception):
+from .general_parser_exception import GenericParsingException
+
+
+class IncorrectMatrixBoundaryException(GenericParsingException):
     """Raised when the matrix boundary of the format [...] is missing"""
-    pass
+    base_msg = 'Missing Or Incorrect Matrix Boundary of Format [...]. '
 
 
-class IncorrectVectorBoundaryException(Exception):
+class IncorrectVectorBoundaryException(GenericParsingException):
     """Raised when the vector boundary of the format [...] is missing"""
-    pass
+    base_msg = 'Missing Or Incorrect Vector Boundary of Format [...]. '
 
 
-class NonnumericValueInMatrixException(Exception):
+class NonnumericValueInMatrixException(GenericParsingException):
     """Raised when non-numeric values found in a matrix definition"""
-    pass
+    base_msg = 'Found Non-Numeric Values in Matrix Definition. '
 
 
-class NonnumericValueInVectorException(Exception):
+class NonnumericValueInVectorException(GenericParsingException):
     """Raised when non-numeric values found in a vector definition"""
-    pass
+    base_msg = 'Found Non-Numeric Values in Vector Definition. '
 
 
-class IncorrectMatrixDefinitionException(Exception):
+class IncorrectMatrixDefinitionException(GenericParsingException):
     """Raised when the matrix does not contain same number of columns for each row"""
-    pass
 
-class MatricesUnequalRowsException(Exception):
+    def __init__(self, row, row_len, temp_len):
+        self.base_msg = f'Row Does Not Have Expected Length: [{row}]. Expected Length: {row_len}, Found: {temp_len}'
+        super().__init__(self.base_msg)
+
+
+class MatricesUnequalRowsException(GenericParsingException):
     """Raised when multiple matrices supposed to be having same number of rows but do not"""
-    pass
 
-class MultipleMatricesSpecifiedException(Exception):
+    def __init__(self, matrix, row_len, temp_len):
+        self.base_msg = f'Matrix Does Not Have Expected Number of Rows: {matrix}. Expected: {row_len}, Found: {temp_len}. '
+        super().__init__(self.base_msg)
+
+
+class MultipleMatricesSpecifiedException(GenericParsingException):
     """Raised when the multiple matrices is specified when a single matrix is expected"""
-    pass
+    base_msg = 'Expected Single Matrix, Found Multiple. '
 
 
-class MatrixNotQuadraticException(Exception):
+class MatrixNotQuadraticException(GenericParsingException):
     """Raised when the input matrix not quadratic"""
-    pass
+    base_msg = 'Input Matrix Is Not Quadratic. '
