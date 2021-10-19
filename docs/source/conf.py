c05a0267da85cb871777bceef23b4ba5c6993993
Ivo <I.vanStraalen@student.tudelft.nl>
2021-10-19 16:46:53 +0200

    Change theme to rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -88,7 +88,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 # import sphinx_pdj_theme
 # html_theme = 'sphinx_pdj_theme'
 # html_theme_path = [sphinx_pdj_theme.get_html_theme_path()]
