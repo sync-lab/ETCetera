af441b9936780ee4d6fa1b315aa7712e2f4dd62a
Ivo <I.vanStraalen@student.tudelft.nl>
2021-10-25 20:31:17 +0200

    Add warning for when grid_points_per_dim is specified when not necessary

--- sentient/Abstractions/NonlinearETC/TrafficModelNonlinearETC.py
+++ sentient/Abstractions/NonlinearETC/TrafficModelNonlinearETC.py
@@ -202,7 +202,6 @@ class TrafficModelNonlinearETC(Abstraction):
         self.Regions = None
         self.Grid = None
 
-
         # Setting solver options
         if manifolds_times is not None:
             manifolds_times.sort()
@@ -249,6 +248,10 @@ class TrafficModelNonlinearETC(Abstraction):
         self.parallel = parallel
         if parallel:
             print("Warning: parallelization is still being tested, so use at your own risk..")
+
+        if not self.Homogenization_Flag and partition_method == 'manifold' and grid_points_per_dim is not None:
+            logging.warning('For homogeneous systems with partition manifold, the parameter grid points per dimension is not used.')
+            print('For homogeneous systems with partition manifold, the parameter grid points per dimension is not used.')
 
         logging.info(f'Original State Vector: {original_state}')
         logging.info(f'Original Dynamics: {original_dynamics}')
