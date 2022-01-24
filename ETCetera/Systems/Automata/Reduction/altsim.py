df92d29c6b25e526789261e578aafae11cad0aa3
ggleizer <ggleizer@gmail.com>
2022-01-24 10:56:53 +0100

    Organizing, adding docstrings

--- ETCetera/Systems/Automata/Reduction/altsim.py
+++ ETCetera/Systems/Automata/Reduction/altsim.py
@@ -3,7 +3,7 @@
 """
 Created on Fri Oct  8 10:20:35 2021
 
-@author: ggleizer
+@authors: ggleizer, kmadnani
 """
 
 from itertools import product, combinations, permutations
@@ -14,34 +14,146 @@ import logging
 ''' alur style '''
 
 
-def minimize_alternating_simulation_equivalence(S, H, X0, Hrel=None):
+def minimize_alternating_simulation_equivalence(S, H, X0):
+    """Minimize system (S,H,X0) modulo Alternating Simulation Equivalence.
+
+    Runs the algorithm presented in the paper "A simpler alternative:
+    minimizing transition systems modulo alternating simulation equivalence."
+
+
+    Parameters
+    ----------
+    S : dictionaty of dictionaries X -> (U -> 2^X)
+        Function mapping states to a function mapping inputs to state sets.
+        This is a convenient way of describing the transition function for
+        our implementation. Every state in X must be in S, even if its value
+        is an empty dictionary (i.e., S.keys() must return the whole state set)
+    H : dictionary (X -> Y)
+        Function mapping states to outputs
+    X0 : set (subset of X)
+        Initial state set.
+
+    Returns
+    -------
+    Sr : dictionary of dictionaries X -> (U -> 2^X)
+        Transition map of the minimal system
+    HQ : dictionary (X -> Y)
+        Output map of the minimal system
+    X0r : set (subset of X)
+        Initial state set of the minimal system
+
+    """
+
+    # Step 0
     logging.info('Computing maximal alternating simulation relation')
-    R = maximal_alternating_simulation_relation(S, H, Hrel=Hrel)
+    R = maximal_alternating_simulation_relation(S, H)
+
+    # Step 1
     logging.info('Obtaining the quotient system')
     SQ, HQ, simulated, RQ, S0, X0Q = make_quotient(S, H, R, X0)
+
+    # Step 2
     logging.info('Removing irrational controller actions')
     Sr = remove_controller_actions(SQ, RQ)
+
+    # Steps 3 and 4 are done together for efficiency
     logging.info('Removing irrational environment actions and unreachable'
                  ' states')
     X0r = remove_initial_states(Sr, RQ, X0Q)
     Sr = delete_transitions(Sr, simulated, X0r)
+
+    # Rebuild output map by eliminating removed states
     HQ = {q:y for q,y in HQ.items() if q in Sr}
+
     return Sr, HQ, X0r
 
 
-def initial_alternating_sim(H, H2, Hrel=None):
-    #print(Hrel)
-    if Hrel is None:
-        return set((x1,x2) for x1,y1 in H.items() for x2,y2 in H2.items()
-                   if y1 == y2)
-    else:
-        return set((x1,x2) for x1,y1 in H.items() for x2,y2 in H2.items()
-                   if (y1,y2) in Hrel)
+def maximal_alternating_simulation_relation(S, H, S2=None, H2=None):
+    """Compute the maximal alternating simulation relation from (S,H) to
+    (S2,H2).
+
+    If S2 and H2 are None, compute the maximal alternating simulation
+    relation from (S,H) to itself.
+
+    This is the most naive algorithm to compute the maximal alternating
+    simulation relation between two systems. It is just a fixed point
+    iteration starting from the relation R0 s.t. (x,x') in R0 iff
+    H(x) = H'(x'), then removing pairs from the realtion by checking if
+    they violate the alternating simulation relation conditions.
+
+    Note that the maximal alternating simulation relation is oblivious to
+    the initial state sets as long as H(X0) is a subset of H2(X02)
+    (otherwise, S2 is not an alternating simulation of S). This is trivially
+    satisfied when computing the relation from S to itself.
+
+
+    Parameters
+    ----------
+    S and H are the transition and output maps as in the main functiion
+    minimize_alternating_simulation_equivalence. S2 and H2 are optional.
+
+    Returns
+    -------
+    R : set
+        Maximal alternating simulation relation from (S,H) to (S2,H2). A
+        subset of X x X2.
+
+    """
+    if S2 is None:
+        S2 = S
+        H2 = H
+
+    R = initial_alternating_sim(H, H2)
+
+    while True:
+        Rnew = set()
+        for (xa, xb) in R:
+            if is_alternating(xa, xb, R, S, S2):
+                Rnew.add((xa,xb))
+        if Rnew == R:
+            break
+        R = Rnew.copy()
+
+    return R
+
+
+def initial_alternating_sim(H, H2):
+    """ See maximal_alternating_simulation_relation """
+    return set((x1,x2) for x1,y1 in H.items() for x2,y2 in H2.items()
+               if y1 == y2)
 
 
 def is_alternating(xa, xb, R, Sa, Sb):
+    """Check if (xa,xb) of systems (Sa,Sb) satisfy the conditions for
+    alternating simulation given relation R.
+
+    In fact we only check condition 3:
+        forall ua in Sa[xa] there exists ub in Sb[xb] s.t.
+        forall xb' in Sb[xb][ub] there exists xa' in Sa[xa][ua] s.t.
+        (xa', xb') in R
+
+    We assume conditions 1 and 2 are satisfied.
+
+
+    Parameters
+    ----------
+    xa : element of Sa.keys()
+        A state of Sa
+    xb : element of Sb.keys()
+        A state of Sb.
+    R : set
+        The candidate alternating simulation relation from Sa to Sb.
+    Sa and Sb: dictionary of dictionaries X -> (U -> 2^X)
+        See minimize_alternating_simulation_equivalence
+
+    Returns
+    -------
+    bool
+        True if (xa,xb) satisfy conditions 3.
+
+    """
+
     # Verify if (xa,xb) satisfy condition 3.
-    # It assumes conditions 1 and 2 hold
     for ua, post_ua in Sa[xa].items():
         for ub, post_ub in Sb[xb].items():
             for xbp in post_ub:
@@ -55,25 +167,6 @@ def is_alternating(xa, xb, R, Sa, Sb):
             return False
     # If we reach here, then every ua worked
     return True
-
-
-def maximal_alternating_simulation_relation(S, H, Hrel=None, S2=None, H2=None):
-    if S2 is None:
-        S2 = S
-        H2 = H
-
-    R = initial_alternating_sim(H, H2, Hrel=Hrel)
-
-    while True:
-        Rnew = set()
-        for (xa, xb) in R:
-            if is_alternating(xa, xb, R, S, S2):
-                Rnew.add((xa,xb))
-        if Rnew == R:
-            break
-        R = Rnew.copy()
-
-    return R
 
 
 def make_quotient(S, H, R, X0=None):
@@ -246,19 +339,10 @@ def stats(S, X0):
 
 
 if __name__ == '__main__':
-    ''' Some test data '''
-
-    B = {'q01' : {'w': {'q02'}, 's': {'q01'}},
-     'q02' : {'s': {'q01', 'q11'}},
-     'q11' : {'w': {'q12'}, 's': {'q11'}},
-     'q12' : {'w': {'q13'}, 's': {'q11'}},
-     'q13' : {'s': {'q11'}}
-     }
-
-    H = {'q01': 'T', 'q02': 'W', 'q11': 'T', 'q12' : 'W', 'q13': 'W'}
+    ''' The running example from the paper '''
 
 
-    Borig = {'q01' : {'w': {'q02'}, 's': {'q01'}},
+    Sorig = {'q01' : {'w': {'q02'}, 's': {'q01'}},
              'q02' : {'w': {'q01', 'q11'}, 's': {'q01', 'q11'}},
              'q11' : {'w': {'q12'}, 's': {'q11'}},
              'q12' : {'w': {'q13'}, 's': {'q11'}},
@@ -270,11 +354,11 @@ if __name__ == '__main__':
 
     X0orig = {'q01', 'q11'}
 
-    n, n0, m = stats(Borig, X0orig)
+    n, n0, m = stats(Sorig, X0orig)
     print(f'Original system: {n} states ({n0} initial) and {m} transitions.')
-    print(Borig)
+    print(Sorig)
 
-    Sr, HQ, X0r = minimize_alternating_simulation_equivalence(Borig,
+    Sr, HQ, X0r = minimize_alternating_simulation_equivalence(Sorig,
                                                               Horig,
                                                               X0orig)
 
