f2ec3a6d99f3008a51c6279f2189b5829a2fd34c
ggleizer <ggleizer@gmail.com>
2022-02-11 22:21:00 +0100

    Adding 5 system scheduling example'

---
+++ examples/scheduling_safetygame_5sys_4d.py
@@ -0,0 +1,47 @@
+# Loading the traffic models
+import sys, os, logging, time
+from datetime import timedelta
+
+logging.getLogger().setLevel(logging.INFO)
+
+# Check if examples/example_linearpetc.py has been run, as its result is used here
+from config import root_path
+if not os.path.exists(os.path.join(root_path, 'saves/ifac1.pickle')):
+    print('Run "python3 etc2traffic.py -o ifac1.json linear examples/linear_ifac2020.txt"'
+          ' first, as the resulting traffic model is used here!')
+    sys.exit()
+if not os.path.exists(os.path.join(root_path, 'saves/ifac2.pickle')):
+    print('Run "python3 etc2traffic.py -o ifac2.json linear examples/linear_ifac2020_2.txt"'
+          ' first, as the resulting traffic model is used here!')
+    sys.exit()
+
+import ETCetera.Abstractions as abstr
+traffic1 = abstr.TrafficModelLinearPETC.from_bytestream_file('ifac1.pickle')
+traffic2 = abstr.TrafficModelLinearPETC.from_bytestream_file('ifac2.pickle')
+
+# Scheduling by solving a safety game
+import ETCetera.Scheduling.fpiter as sched
+
+# With BDDs
+cl1 = sched.controlloop(traffic1, use_bdd=True)
+cl2 = sched.controlloop(traffic1, use_bdd=True)
+cl3 = sched.controlloop(traffic1, use_bdd=True)
+cl4 = sched.controlloop(traffic1, use_bdd=True)
+cl5 = sched.controlloop(traffic2, use_bdd=True)
+S = sched.system([cl1, cl2, cl3, cl4, cl5])
+
+now = time.process_time()
+Ux, _ = S.generate_safety_scheduler()  # Scheduler
+elapsed = timedelta(seconds=time.process_time() - now)
+print('Elapsed time on synthesis:', elapsed)
+
+
+# With BDDs
+cl6 = sched.controlloop(traffic2, use_bdd=True)
+S2 = sched.system([cl1, cl2, cl3, cl4, cl5, cl6])
+S2.bdd.configure(reordering=False)
+
+now = time.process_time()
+Ux2, _ = S2.gen_safety_scheduler_basic()  # Scheduler
+elapsed = timedelta(seconds=time.process_time() - now)
+print('Elapsed time on synthesis:', elapsed)
